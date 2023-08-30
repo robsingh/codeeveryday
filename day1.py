b4e07ab80543ece9a2ff6def73a133aca2893425
robsingh <robie10895@gmail.com>
2023-08-30 16:15:56 -0700

    Initial Commit

---
+++ day1.py
@@ -0,0 +1,7 @@
+"""
+Write a Python function that calculates the sum of a list of numbers:
+
+The function should accept a list of numbers and return the sum of those numbers.
+If no argument is provided (that is, numbers is None), return the sum of the numbers 1 to 100 
+(Note that this is not the same as an empty list of numbers being passed in. In that case the sum returned will be 0).
+"""
