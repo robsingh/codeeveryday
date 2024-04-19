e0d16ec02b53cd2ae11554af26dedcf00aa95f0f
robsingh <robie10895@gmail.com>
2024-04-19 14:27:30 -0400

    final commit, will update soon

---
+++ contextmanager.py
@@ -0,0 +1,13 @@
+"""
+Write a context manager to roll back a transaction on the given Account class.
+
+There are two special (aka dunder) methods you need to define to create a context manager.
+
+The purpose of the context manager is to roll back any transaction that will make the balance go below 0 (debt != cool). 
+
+Good luck and keep calm and code in Python!
+"""
+"""
+
+"""
+
